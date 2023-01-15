1aa6b0de98d772a1e8cf5d0efdf1ecfc82f92ca6
diegoram23 <diego.pta.oc@gmail.com>
2023-01-15 11:16:58 -0800

    API retrieving data correctly and displaying to page

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ const inputPattern = document.getElementById('input-pattern')
 getSchemeBtn.addEventListener('click', getColor)
 
 
-function getColor(){
-    const hexValue = inputColor.value
+function getColor() {
+    const hexValue = inputColor.value.slice(1)
     const schemeMode = inputPattern.value
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic')
-.then(res => res.json())
-.then(data => console.log(data))
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexValue}&mode=${schemeMode}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let html =''
+            for(let color of data.colors){
+                html += `
+                <div class='color-palette'style='background:${color.hex.value}'>
+                <div class='color-names'>
+                <p>${color.hex.value}</p>
+                </div>
+                </div>
+                `
+            }
+            document.getElementById('content').innerHTML = html
+        })
+
 
 }
+
+//calls the default display
+function render() {
+getColor()
+}
+render()
